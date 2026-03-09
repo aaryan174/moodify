a840995ea2a9f18fcdf8fb9a644b37f4256f01ff
Aryan Patel <aryan007.lko@gmail.com>
2026-03-09 12:07:07 +0530

    Implement user registration and login functionality with JWT authentication

---
+++ Backend/src/controller/user.controller.js
@@ -0,0 +1,90 @@
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
+const userModel = require("../model/user.model")
+
+
+
+async function registerController(req, res) {
+    const {username, email, password} = req.body;
+    
+    const isUserAlreadyExists = await userModel.findOne({
+        $or:[
+            {username},
+           {email}
+        ]
+    })
+
+    if(isUserAlreadyExists){
+        return res.status(400).json({
+            message: "user already exists"
+        })
+    }
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const user = await userModel.create({
+        username, email, password:hashPassword
+    })
+
+    const token = jwt.sign({
+        id: user._id,
+        username: user.username
+    }, process.env.TOKEN,{ expiresIn: "3d"})
+
+    res.cookie("token",token)
+
+   return res.status(201).json({
+        message:"user register succussfully",
+        user:{
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+    })
+}
+
+
+async function loginController(req, res) {
+    const {username, email, password} = req.body
+
+    const isUserExists = await userModel.findOne({
+        $or:[
+            {username},
+            {email}
+        ]
+    })
+    if(!isUserExists){
+        return res.status(400).json({
+            message:"Invalid credential"
+        })
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if(!isPasswordValid){
+        return res.status(400).json({
+            message:"Invalid credentials"
+        })
+    }
+    const token = jwt.sign({
+        id: user._id,
+        username: user.username
+    }, process.env.TOKEN, {expiresIn: "3d"})
+
+    res.cookie("token", token)
+
+    return res.status(200).json({
+        message:"user logged in succussfully",
+        user:{
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+    })
+}
+
+
+
+module.exports ={
+    registerController,
+    loginController
+}
--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,1 +1,14 @@
- 
+const express = require("express");
+const { registerController } = require("../controller/user.controller");
+
+const Router = express.Router();
+
+
+
+// register user POST ROUTE
+Router.post("/register", registerController)
+
+
+
+
+module.exports = Router
